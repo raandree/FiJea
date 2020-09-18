78baaf4d94b1e9b80cfea9ea8e56507b521c7055
Raimund Andree <r.andree@live.com>
2020-09-18 16:49:14 +0200

    Fixes to make it work in dev

--- UD/JeaTabs.ps1
+++ UD/JeaTabs.ps1
@@ -2,21 +2,26 @@ New-UDDashboard -Title "JeaTabs" -Content {
     $user = 'contoso\install'
     $cred = New-Object pscredential($user, ('Somepass1' | ConvertTo-SecureString -AsPlainText -Force))
     $session:jeaServer = 'fiweb1'
-    $session:endpoints = Invoke-Command -ComputerName $session:jeaServer -ConfigurationName JeaDiscovery -ScriptBlock { Get-JeaEndpoint }
+    # JEA MODE
+    #$session:endpoints = Invoke-Command -ComputerName $session:jeaServer -ConfigurationName JeaDiscovery -ScriptBlock { Get-JeaEndpoint }
+    # LOCAL DEV MODE
+    $session:endpoints = @(
+        [PSCustomObject]@{ Name = 'Local 1' },
+        [PSCustomObject]@{ Name = 'Local 2' }
+    )
 
     New-UDTypography -Text "Current process is $PID, current user is '$user'"
     
     New-UDTabs -Tabs {
+        #Wait-Debugger
         foreach ($endpoint in $session:endpoints) {
             New-UDTab -Text $endpoint.Name -Content {
-                $tasks = 
-
-                # JEA MODE
+                 # JEA MODE
                 #$session:tasks = Invoke-Command -ComputerName $session:jeaServer -ConfigurationName JeaDiscovery -ScriptBlock {
                 #    Get-PSSessionCapability -ConfigurationName $args[0] -Username $args[1] | Select-Object -Property Name, Parameters, CommandType
                 #} -ArgumentList $endpoint.Name, $user
                 # LOCAL DEV MODE
-                $session:tasks = Get-Command Set-Content -CommandType Cmdlet | Where-Object { $_.Parameters } | Select-Object -First 550 -Property Name, Parameters, CommandType
+                $session:tasks = Get-Command -Name Set-Content -CommandType Cmdlet | Where-Object { $_.Parameters } | Select-Object -First 550 -Property Name, Parameters, CommandType
 
                 New-UDDynamic -Id dyn1 -Content {
                     $columns = @(
@@ -46,6 +51,10 @@ New-UDDashboard -Title "JeaTabs" -Content {
                                         }
 
                                         $session:parameterElements
+
+                                        New-UDElement -Tag p -Attributes @{'class' = 'right-align' } -Content {
+                                            New-UDButton -Text "Close" -Icon (New-UDIcon -Icon stop) -OnClick { Hide-UDModal }
+                                        }
                                         
                                     } -OnSubmit {
                                         $param = @{}
@@ -76,8 +85,8 @@ New-UDDashboard -Title "JeaTabs" -Content {
                         }
                     )
                 
-                    New-UdTable -Data $tasks -Columns $columns -Id tbl -Sort -Filter -Search
-                } #-AutoRefresh
+                    New-UdTable -Data $session:tasks -Columns $columns -Id tbl -Sort -Filter -Search
+                }
             } -Stacked -Id dyn1
         }
     } -RenderOnActive        
